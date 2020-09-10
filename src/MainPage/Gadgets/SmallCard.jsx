f97082ee8081f369acae5b45b658a9fee5f3d640
LizzardMedeiros <luis.medeiros@outlook.com.br>
2020-09-09 22:10:46 -0300

    CC

---
+++ src/MainPage/Gadgets/SmallCard.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const SmallCards = ({ title, onClick }) => (
+  <button
+    type="button"
+    className="small-card"
+    data-testid={`${title}-category-filter`}
+    onClick={() => { onClick(title); }}
+  >
+    { title }
+  </button>
+);
+
+SmallCards.propTypes = {
+  title: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
+};
+
+SmallCards.defaultProps = {
+  onClick: () => {},
+};
+
+export default SmallCards;
--- src/MainPage/MainDrinks.jsx
+++ src/MainPage/MainDrinks.jsx
@@ -1,29 +1,8 @@
 import React, { useEffect, useState, useContext } from 'react';
-import PropTypes from 'prop-types';
 import AppContext from '../Context/AppContext';
 import api from '../Services/FetchAPI';
 import Card from '../Components/Card';
-
-const SmallCards = ({ title, onClick }) => (
-  <button
-    type="button"
-    className="small-card"
-    data-testid={`${title}-category-filter`}
-    onClick={() => { onClick(title); }}
-  >
-    { title }
-  </button>
-);
-
-const RenderCategories = ({ categories, getValue }) => (
-  categories.map(({ strCategory, idCategory }) => (
-    <SmallCards
-      key={idCategory}
-      title={strCategory}
-      onClick={(value) => getValue(value)}
-    />),
-  )
-);
+import RenderCategories from './Gadgets/RenderCategories';
 
 const MainDrinks = () => {
   const { fetch, bebidas12 } = useContext(AppContext);
@@ -70,24 +49,6 @@ const MainDrinks = () => {
       </div>
     )
   );
-};
-
-SmallCards.propTypes = {
-  title: PropTypes.string.isRequired,
-  onClick: PropTypes.func,
-};
-
-RenderCategories.propTypes = {
-  categories: PropTypes.arrayOf(PropTypes.object).isRequired,
-  getValue: PropTypes.func,
-};
-
-SmallCards.defaultProps = {
-  onClick: () => {},
-};
-
-RenderCategories.defaultProps = {
-  categories: [],
 };
 
 export default MainDrinks;
